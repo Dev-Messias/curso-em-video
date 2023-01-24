6926a688856ca42fe38db4a8226b1a846c73c796
DevMessias <messiasbento944@gmail.com>
2023-01-24 12:32:06 -0300

    Condições ( parte 1 )

---
+++ javascript/aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60;
+console.log(`Velocidade do carro ${vel}`);
+// Condição simples
+if(vel > 60){
+    console.log(`Você ultrapassou da velocidade permita. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
